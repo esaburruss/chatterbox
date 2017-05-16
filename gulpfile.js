888ad86d847f3cfc9559f9b73c45f8f99833a71b
Edward Burruss <esaburruss@Edwards-MacBook-Pro.local>
2017-05-16 16:44:16 -0400

    End of day Commit

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,14 @@ var minifycss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var gzip = require('gulp-gzip');
 var livereload = require('gulp-livereload');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function(){
+  connect.server({
+    root: 'public',
+    livereload: true
+  });
+});
 
 gulp.task('sass', function() {
     return gulp.src('assets/styles/*.scss')
@@ -14,4 +22,14 @@ gulp.task('sass', function() {
         .pipe(minifycss())
         .pipe(gulp.dest('static/css'))
         .pipe(livereload());
+});
+
+gulp.task('livereload', function (){
+  gulp.src('./static/**/*')
+  .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./assets/styles/*.scss', ['sass']);
+  gulp.watch('./static/**/*', ['livereload']);
 });
