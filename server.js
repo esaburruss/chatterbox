6f90fddd4f8405115096e0aac1c6d99ef368ff0c
Edward Burruss <esaburruss@Edwards-MacBook-Pro.local>
2017-05-17 18:17:31 -0400

    End of day

--- server.js
+++ server.js
@@ -8,17 +8,20 @@ var server = app.listen( port, function(){
 });
 
 var io = require('socket.io').listen(server);
-
+var directory = [];
 io.on('connection', function(socket){
   var username;
-  var pk
+  var pk;
+  var directoryId;
+  var directoryObj;
   socket.on('profile', function(msg){
     //console.log('user ' + msg.username + ' has signed in');
     //io.emit('chat-message', msg);
   });
 
   socket.on('chat-message', function(msg){
-    console.log('message: ' + msg);
+    console.log('message: ' + msg.msg);
+    //var chatMsg = ''
     io.emit('chat-message', msg);
   });
 
@@ -26,8 +29,24 @@ io.on('connection', function(socket){
       console.log('User[' + pk +'] ' + username + ' has disconnected');
       //var i = allClients.indexOf(socket);
       //allClients.splice(i, 1);
+      //let obj = directory.find(x => x.name == username);
+      let index = directory.indexOf(directoryObj);
+      console.log(index);
+      directory.splice(index, 1);
+      printDirectory();
    });
    pk = socket.handshake.query.pk;
    username = socket.handshake.query.username;
    console.log('User[' + socket.handshake.query.pk +'] ' +socket.handshake.query.username + ' has logged in');
+   //console.log(socket.id)
+   directoryObj = {clientId:socket.id, username:username, id:pk};
+   directory.push(directoryObj);
+   console.log(directory.length);
+   printDirectory();
 });
+
+function printDirectory() {
+  for(x in directory) {
+    console.log(directory[x].username);
+  }
+}
